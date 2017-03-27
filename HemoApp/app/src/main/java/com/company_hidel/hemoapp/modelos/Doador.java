8772a0f78d37f60fda058511f1e132bb50b42c4a
H1d3l <hidels.rios@gmail.com>
2017-03-27 17:41:58 -0300

    atualização 4
    add-função-Salvar doador

---
+++ HemoApp/app/src/main/java/com/company_hidel/hemoapp/modelos/Doador.java
@@ -0,0 +1,40 @@
+package com.company_hidel.hemoapp.modelos;
+
+import com.orm.SugarRecord;
+
+/**
+ * Created by hidel on 27/03/2017.
+ */
+
+public class Doador extends SugarRecord{
+    private String nome;
+    private String idade;
+    private String sexo;
+    private String telefone;
+    private String tipo_sanguineo;
+
+
+    public Doador(){
+
+    }
+
+    public Doador(String nome,String idade,String sexo,String telefone,String tipo_sanguineo){
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.telefone = telefone;
+        this.tipo_sanguineo = tipo_sanguineo;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Doador{" +
+                "nome='" + nome + '\'' +
+                ", idade='" + idade + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", tipo_sanguineo='" + tipo_sanguineo + '\'' +
+                '}';
+    }
+}
---
+++ HemoApp/app/src/main/java/com/company_hidel/hemoapp/modelos/Hospital.java
@@ -0,0 +1,37 @@
+package com.company_hidel.hemoapp.modelos;
+
+import com.orm.SugarRecord;
+import com.orm.dsl.Ignore;
+
+import java.util.List;
+
+/**
+ * Created by hidel on 27/03/2017.
+ */
+
+public class Hospital extends SugarRecord{
+    private String nome_Hospital;
+    private String rua;
+    private int numero;
+    private String cidade;
+    private String uf;
+    private int telefone;
+    private String email;
+    @Ignore
+    private List<Doador> doadores_cadastrados;
+
+
+    public Hospital(){
+
+    }
+
+    public Hospital(String nome,String rua,int numero,String cidade,String uf,int telefone,String email){
+        this.nome_Hospital = nome;
+        this.rua = rua;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.telefone = telefone;
+        this.email = email;
+
+    }
+}
